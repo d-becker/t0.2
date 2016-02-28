b97b4ac0dba1943c89cb9356cfd702589de6e143
Daniel Becker <beckerdaniel.dani@gmail.com>
2016-02-28 21:50:14 +0100

    Forgot to add modifications to the previous commit.

--- include/locking_shared_ptr.h
+++ include/locking_shared_ptr.h
@@ -32,6 +32,14 @@ namespace tetris {
  * To facilitate access the the object pointed to, \c operator*
  * and \c operator-> are overridden.
  *
+ * Note that \c locking_shared_ptr is not copy-constructible
+ * but it is move-constructible.
+ *
+ * The behaviour is undefined if the thread that destructs
+ * the \c locking_shared_ptr is not the same as the one that created it,
+ * therefore objects of this class should always be allocated on the stack
+ * and never dynamically on the heap.
+ *
  * \param T The type of the object pointed to.
  * \param Mutex The type of the mutex that will be used.
  *        The default is \c std::mutex.
@@ -40,6 +48,18 @@ template<typename T, typename Mutex = std::mutex>
 class locking_shared_ptr
 {
   public:
+    /**
+     * Constructs a new \c locking_shared_ptr from
+     * the provided \c std::shared_ptr and locks the provided mutex.
+     *
+     * The behaviour is undefined if the thread that destructs
+     * the \c locking_shared_ptr is not the same that created it.
+     *
+     * \param ptr A shared pointer to the object that
+     *        this \c locking _shared_ptr is going to point to.
+     * \param mutex The mutex that is locked and will be held
+     *        by this \c locking_shared_ptr during its lifetime.
+     */
     locking_shared_ptr(const std::shared_ptr<T>& ptr, Mutex& mutex)
       : m_shared_ptr(ptr),
         m_lock(mutex)
@@ -47,6 +67,11 @@ class locking_shared_ptr
       m_lock.lock();
     }
 
+    /**
+     * Destructs the \c locking_shared_ptr and unlocks the mutex that it held.
+     * The behaviour is undefined if the thread that destructs
+     * the \c locking_shared_ptr is not the same as the one that created it.
+     */
     virtual ~locking_shared_ptr()
     {
       m_lock.unlock();
@@ -54,6 +79,9 @@ class locking_shared_ptr
 
     locking_shared_ptr(const locking_shared_ptr& other) = delete;
 
+    /**
+     * Move-constructs a \c locking_shared_ptr.
+     */
     locking_shared_ptr(locking_shared_ptr&& other)
       : m_shared_ptr(other.m_shared_ptr),
         m_lock(other.m_lock)
@@ -61,10 +89,22 @@ class locking_shared_ptr
 
     }
 
+    /**
+     * Accesses the object pointed to. The behaviour is undefined if the
+     * underlying pointer is null.
+     *
+     * \return A reference to the object pointed to.
+     */
     T& operator*() const {
       return *m_shared_ptr;
     }
 
+    /**
+     * Accesses the object pointed to. The behaviour is undefined if the
+     * underlying pointer is null.
+     *
+     * \return A pointer to the object pointed to.
+     */
     T* operator->() const {
       return m_shared_ptr.get();
     }
